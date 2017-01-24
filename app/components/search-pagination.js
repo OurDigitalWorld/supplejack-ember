94f0c6eba09abf9359ab739c73a6be9655e2a6c4
revanar <m.matt.barry@gmail.com>
2017-01-24 11:55:14 -0500

    updated search-pagination to have bootstrap styling, and improved the logic it uses so that it will always consist of 13 objects (including forward/back buttons, ellipses, and page links).

--- app/components/search-pagination.js
+++ app/components/search-pagination.js
@@ -3,15 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   //default variables
   display: 'both',
-  offsetAmount: '',
-  init() {
+  offsetAmount: 20,
+
+  didReceiveAttrs() {
+    //sets the offsetamount to be equal to the per_page value
     this._super(...arguments);
     this.set('offsetAmount', this.get('meta.per_page'));
   },
-  //computed value of last page that will contain content
-  lastPage: Ember.computed('meta', function(){
-    return Math.ceil(this.get('meta.result_count') / this.get('meta.per_page'));
-  }),
   //computed value of current record offset based on page
   offset: Ember.computed('meta', function(){
     return ((this.get('meta.page') - 1) * this.get('meta.per_page') + 1);
@@ -27,12 +25,56 @@ export default Ember.Component.extend({
       return this.get('meta.result_count');
     }
   }),
+  //list of options for the results per page to be set to
   offsetOptions: [
     {value: 10},
     {value: 20},
     {value: 50},
     {value: 100},
   ],
+  //computed value of last page that will contain content
+  lastPage: Ember.computed('meta', function(){
+    return Math.ceil(this.get('meta.result_count') / this.get('meta.per_page'));
+  }),
+  //determines whether the first ellipse in the pagination bar needs to be displayed
+  displayStartEllipse: Ember.computed('meta', 'lastPage', function(){
+    return (this.get('meta.page') >= 7 && this.get('lastPage') > 11);
+  }),
+  //determines whether the second ellipse in the pagination bar needs to be displayed
+  displayEndEllipse: Ember.computed('meta', 'lastPage', function(){
+    return (this.get('meta.page') <= (this.get('lastPage') - 6 ) && this.get('lastPage') > 11);
+  }),
+  //returns an object containing all of the pagination links that should be
+  // displayed aside from the first and last page links.
+  paginationLinks: Ember.computed('meta', 'lastPage', function(){
+    let output = [];
+    const current = this.get('meta.page');
+    const last = this.get('lastPage');
+    //function that takes an initial and maximum value,
+    //and returns a page object for every number in between.
+    //also assigns property 'isActive' to the current page.
+    const getPage = (init, max)=>{
+      for (let i=init; i<=max; i++){
+        let link = {};
+        if (i === current) link.isActive = true;
+        link.page = i;
+        output.push(link);
+      }
+    };
+    //if there are 11 or fewer pages
+    // get pages 2 to (last -1)
+    if (last <= 11) getPage(2, (last-1));
+      //if the current page is less than 7
+      //get pages 2 to 9
+    else if (current < 7) getPage(2, 9);
+      //if the current page is within 7 of the last page
+      //get pages (last-9) to (last-1)
+    else if (current > (last - 6)) getPage((last-8),(last-1));
+      //otherwise, just get the 3 pages on either side of the current page
+    else getPage((current-3),(current+3));
+
+    return output;
+  }),
   actions: {
     //function for changing which page is showing when you change the per_page value
     // ensures that the same results stay near the top of the page
