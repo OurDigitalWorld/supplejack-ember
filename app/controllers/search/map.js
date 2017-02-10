2d3d2561dd9ae46f099b31599f90963c1c5e6f42
revanar <m.matt.barry@gmail.com>
2017-02-10 17:17:34 -0500

    added map component.  Once we figure out how to do geo_bbox searches, the map can be populated!

--- app/controllers/search/map.js
+++ app/controllers/search/map.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   application: Ember.inject.controller('application'),
   //leaflet variables - should eventually be moved to new controller
-  lat: 57.231502991478926,
-  lng: -96.6796875,
-  zoom: 3,
+  lat: 53.014783245859235,
+  lng: -96.24023437500001,
+  zoom: 4,
   actions: {
     updateParams(obj){
       this.get('application').send('updateParams', obj);
