1a7b9027edb934fb8e6d0c2a196aa0d45b41aa58
revanar <m.matt.barry@gmail.com>
2017-02-24 11:59:57 -0500

    added a function to the router that hides the 'back-to-top' button on pages that don't have a scrollbar. It feels wrong to put the function in the router instead of in the back-to-top button component, but it's the only place with a hook that fires on every route transition.

--- app/router.js
+++ app/router.js
@@ -4,7 +4,19 @@ import RouterScroll from 'ember-router-scroll';
 
 const Router = Ember.Router.extend(RouterScroll, {
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+
+  //a bit of a kludge, but this makes the return-to-top button hidden on pages that don't scroll.
+  //I wish I could put this in the component, but this is the only place where I could access a hook that fires on every route change.
+  didTransition(){
+    Ember.run.later(this, function(){
+      if (document.body.scrollHeight > document.body.clientHeight){
+        Ember.$('#return-to-top').removeClass('hidden');
+      } else {
+        Ember.$('#return-to-top').addClass('hidden');
+      }
+    }, 10)
+  }
 });
 
 Router.map(function() {
