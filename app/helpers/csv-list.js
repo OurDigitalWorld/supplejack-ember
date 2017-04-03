705f9e0144c546f7694c12e84e90667661bac03c
revanar <m.matt.barry@gmail.com>
2017-04-03 10:08:26 -0400

    changed 'unknown' variable used by csv-list class to 'empty' to be more semantically clear.  Added a bit of documentation

--- app/helpers/csv-list.js
+++ app/helpers/csv-list.js
@@ -1,8 +1,10 @@
 //HOW TO USE - use this helper when you want to format an array of strings into a comma separated list.
-//Syntax: {{csv-list array}}
-// where array is replaced by the array of your choice.
+//Syntax: {{csv-list array empty="Unknown"}}
+// array : replaced by the array of your choice.
+// empty (optional) : the value you want this helper to return if it's passed something empty or undefined.
 //Output: If given a single string, outputs whatever it recieved normally.
 //        If given an array of strings, outputs the array as an unordered list with the class "csv"
+//        If given nothing, it returns an empty string or the value passed to the 'empty' variable, wrapped in span.empty
 
 import Ember from 'ember';
 
@@ -10,10 +12,10 @@ export function csvList(params, hash) {
   let [data] = params;
   if (!Array.isArray(data)){data = [data];}
   let result = "";
-  let unknown = hash.unknown === undefined ? '' : hash.unknown;
+  let empty = hash.empty === undefined ? '' : hash.empty;
   if (typeof data !== 'undefined'){
     if (data.length === 0){
-      return Ember.String.htmlSafe(`<span class="unknown">${unknown}</span>`);
+      return Ember.String.htmlSafe(`<span class="empty">${empty}</span>`);
     } else if (data.length === 1){
       return data;
     } else if (data.length > 1){
