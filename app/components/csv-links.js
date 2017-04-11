50ca1c37a8ea88ef8b32427e55c70279a9fc9af9
revanar <m.matt.barry@gmail.com>
2017-04-11 15:35:14 -0400

    added fastboot-server for deployment.  To use:
    Clone project onto prod server.
    Run "node fastboot-server.js"

--- app/components/csv-links.js
+++ app/components/csv-links.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 const CsvLinksComponent = Ember.Component.extend({
   dataLength: Ember.computed('data', function() {
     if (typeof this.get('data') !== 'undefined') {
-      console.log(this.get('data'));
-      console.log(this.get('data').length);
       return this.get('data').length;
     }
   })
